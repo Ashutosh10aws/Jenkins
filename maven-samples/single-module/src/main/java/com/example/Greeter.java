e3bd8051e1a995d71255254f83b131856fe8a42a
Ashutosh10aws <69576404+Ashutosh10aws@users.noreply.github.com>
2022-10-25 13:21:06 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("HELLOAWS, %s!", someone);
+    return String.format("GOOGLE, %s!", someone);
   }
 }
